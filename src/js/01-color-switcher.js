4d16dcd9bdc4e32fbd6543d3f28a127dc3974f30
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-08-27 15:56:21 +0300

    Task 01 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+ 
+refs.startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+  document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    onStartBtnClick('disabled', true);
+});
+
+refs.stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    onStopBtnClick('disabled', true);
+});
+
+function onStartBtnClick(attr, value) {
+    refs.startBtn.setAttribute(attr, value);
+    refs.stopBtn.removeAttribute(attr);
+}
+function onStopBtnClick(attr, value) {
+    refs.stopBtn.setAttribute(attr, value);
+    refs.startBtn.removeAttribute(attr);
+}
