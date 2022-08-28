280d3ac7201e52961b0d1009d1a06c2394a52621
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-08-29 00:01:55 +0300

    Add flatpickr

---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const inputEl = document.querySelector("#datetime-picker"); 
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
